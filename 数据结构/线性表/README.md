f2c7ef9094a3876945443634f531ebeeca560084
xxpasswd <1490621113@qq.com>
2019-02-13 10:31:15 +0800

    add queue

--- 数据结构/线性表/README.md
+++ 数据结构/线性表/README.md
@@ -1,1 +1,2 @@
 #### [1. stack.py：栈](./stack.py)
+#### [2. queue.py: 队列](./queue.py)
--- 数据结构/线性表/queue.py
+++ 数据结构/线性表/queue.py
@@ -23,3 +23,55 @@ class Queue:
 
     def size(self):
         return len(self._queue)
+
+'''
+双端队列实现
+
+add_front(item):O(n)
+add_rear(item):O(1)
+remove_front():O(n)
+remove_rear():O(1)
+is_empty():O(1)
+size():O(1)
+'''
+
+class Deque:
+    def __init__(self):
+        self._deque = []
+
+    def add_front(self, item):
+        self._deque.insert(0, item)
+
+    def add_rear(self, item):
+        self._deque.append(item)
+
+    def remove_front(self):
+        return self._deque.pop(0)
+
+    def remove_rear(self):
+        return self._deque.pop()
+
+    def is_empty(self):
+        return self._deque == []
+
+    def size(self):
+        return len(self._deque)
+
+"""
+用双端队列实现回文检测
+"""
+
+def palchecker(astring):
+    chardeque = Deque()
+
+    for i in astring:
+        chardeque.add_rear(i)
+
+    stillequel = True
+    while chardeque.size()>1 and stillequel:
+        front = chardeque.remove_front()
+        rear = chardeque.remove_rear()
+        if front != rear:
+            stillequel = False
+
+    return stillequel
